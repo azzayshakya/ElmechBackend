909a1a41fb89e9ee7f053332db8335b0765a0309
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2025-01-17 01:05:00 +0530

    adding the frontend url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173", // Adjust this to your frontend URL
+        origin: ["http://localhost:5173","https://elmech.live"], // Adjust this to your frontend URL
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
